a0091c5d76f766db7e7a588392df91e3158b8b70
Leonetienne <leonetienne@hotmail.de>
2021-05-18 01:46:06 +0200

    renamed endz

--- example.cpp
+++ example.cpp
@@ -9,17 +9,27 @@
 
 ganzzahl einstieg()
 {
-    std::aus << "Hallo, Welt!" << std::endz;
+    std::aus << "Hallo, Welt!" << std::umbruch;
 
+    // Erstelle vektor
     std::vektor<ganzzahl> vek;
 
+    // Schiebe Werte in den Vektor
     vek.hinten_schieben(99);
     vek.hinten_schieben(2);
     vek.hinten_schieben(56);
     vek.hinten_schieben(112);
 
-    fuer(konstante ganzzahl & i : vek)
-        std::aus << i << std::endz;
+    // Gebe alle Werte aus
+    fuer (konstante ganzzahl  i : vek)
+        std::aus << i << std::umbruch;
+
+    // Entferne die letzten zwei Werte
+    vek.platze_hinten();
+    vek.platze_hinten();
+
+    // Arbeitsspeicher entlasten
+    vek.schrumpfe_um_zu_passen();
 
     gebe_zurueck 0;
 }
